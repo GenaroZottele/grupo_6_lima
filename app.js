605f1d42b7e20c7c9652b295f7564e73a92b3faa
qblassk <g.bulaschevich@gmail.com>
2022-10-13 10:39:09 -0300

    servidor levantado

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+
+const path = require('path');
+const publicFolderPath = path.resolve('public');
+
+app.use(express.static(publicFolderPath));
+
+const APP_PORT = 3000;
+app.listen(APP_PORT, () => {
+    console.log('Servidor funcionando en puerto ' + APP_PORT)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('views/index.html'))
+})
