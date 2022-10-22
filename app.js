910cf5cc5ce93fe4a93e46a81cad00cc4d934324
qblassk <g.bulaschevich@gmail.com>
2022-10-22 20:36:17 -0300

    cambios en app para linkear views

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-const path = require('path');
 const publicFolderPath = path.resolve('public');
+app.use(express.static(publicFolderPath));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
-app.use(express.static(publicFolderPath));
 
 const APP_PORT = 3000;
 app.listen(APP_PORT, () => {
@@ -14,20 +15,25 @@ app.listen(APP_PORT, () => {
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve("views/index.html"))
-})
-app.get("/register", (req, res) => {
-    res.sendFile(path.resolve("views/register.html"))
-})
-app.get("/login", (req, res) => {
+    res.sendFile(path.resolve('views/index.html'))
+});
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.resolve('views/register.html'))
+});
+
+app.get("/login.html", (req, res) => {
     res.sendFile(path.resolve("views/login.html"))
-})
-app.get("/product", (req, res) => {
+});
+
+app.get("/product.html", (req, res) => {
     res.sendFile(path.resolve("views/productDetail.html"))
-})
-app.get("/productcart", (req, res) => {
+});
+
+app.get("/productcart.html", (req, res) => {
     res.sendFile(path.resolve("views/productCart.html"))
-})
+});
+
 app.post("/", (req, res) => {
     return res.send(req.body)
 })
