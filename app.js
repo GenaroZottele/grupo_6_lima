35f36e3f2e79d5ba8f3db23a9f580bd479b7fb1d
Florencia Rivero <113300135+flormrivero@users.noreply.github.com>
2022-10-22 10:08:51 -0300

    App.get y Post

--- app.js
+++ app.js
@@ -16,3 +16,12 @@ app.listen(APP_PORT, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('views/index.html'))
 })
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve("views/register.html"))
+})
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve("views/login.html"))
+})
+app.post("/", (req, res) => {
+    return res.send(req.body)
+})
