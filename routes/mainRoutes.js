a5a9a86599b739941aede6ab324c0273893733ad
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2022-11-06 13:36:32 -0300

    multer para ruta post para subir foto de usuario

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path  = require('path');
+
 const mainController = require('../controllers/mainController');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, path.join(__dirname, '../public/images/userimages'))
+         },
+    filename:  (req, file, cb)=>{
+        const newFileName = 'user-' + Date.now() +  path.extname(file.originalname);
+        cb(null, newFileName)
+    }
+    });
+
+const upload= multer ({storage});
+
 
 router.get('/', mainController.index);
 
 router.get('/register', mainController.register);
+
+router.post('/register', upload.single('user-image'), mainController.register);
 
 router.get('/login', mainController.login);
 
@@ -19,5 +37,6 @@ router.get('/nosotros', mainController.nosotros);
 router.get('/productDetailMain', mainController.productDetailMain)
 
 router.get('/panel', mainController.panel);
+
 
 module.exports = router;
