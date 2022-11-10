19e1488560c61e6d957e2908c81ab3ee3f8b8231
qblassk <g.bulaschevich@gmail.com>
2022-11-10 17:55:24 -0300

    crud

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -6,13 +6,23 @@ const productsController = require('../controllers/productsController');
 // Listado de productos
 router.get('/', productsController.index);
 
-// Creacion de producto (form)
+// Creacion de producto (muestra form)
 router.get('/', productsController.create);
 
-// Accion de creacion 
+// Accion de creacion (a donde envia form)
 router.post('/', productsController.store);
 
 // Detail de un producto
 router.get('/:id/', productsController.detail);
+
+// Edicion productos (muestra form)
+router.get('/:id/', productsController.edit);
+
+// Edicion productos (a donde envia form)
+router.post('/:id/', productsController.update);
+
+// Elimina producto
+router.post('/:id', productsController.destroy);
+
 
 
