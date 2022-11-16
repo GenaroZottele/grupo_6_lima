7897bf45793004c6ee8aed67af6e99379d57cbad
Keko10 <checholabonia@gmail.com>
2022-11-16 17:52:33 -0300

    prueba middleware2

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const app = express();
 
 const mainRoutes = require('./routes/mainRoutes');
 const productsRoutes = require('./routes/productsRoutes');
-
+const logMiddleware = require('./middlewares/logMiddlewares');
 const publicFolderPath = path.resolve('public');
+
+
 app.use(express.static(publicFolderPath));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(logMiddleware);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
