4c607cca53720696ba744a076a4d0179c19c9890
GenaroZottele <genarozottele14@gmail.com>
2023-01-05 11:38:19 -0300

    migrations

---
+++ src/database/migrations/20230105143035-createProductOrderTable.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('product_order', 
+    { id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      product_id: Sequelize.INTEGER,
+      order_id: Sequelize.INTEGER
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+
+  }
+};
