fc2f450534e7fa9d709c3461582f40e87e9f9eb2
Florencia Rivero <flormrivero@gmail.com>
2023-01-15 21:15:03 -0300

    crud

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -7,105 +7,136 @@ let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const db = require('../src/database/models')
 
 const controller = {
-    //1
-    products: (req, res)=>{
-
-        return res.render('productDetail', {products})
+    
+  
+    create: function (req, res) {
+        db.Producto.findAll()
+          .then(function(productos){
+            return res.render('create', {productos: productos});
+          })
 
     },
-
-    //2
-    create: (req, res) =>{
-
-        return res.render('create');
+    save: function (req, res){
+      db.Producto.create({
+        nombre: req.body.nombre,
+        base: req.body.base,
+        aderezo: req.body.aderezo,
+        ingredientes: req.body.ingredientes,
+        imagen: '/images/'+ req.file.filename,
+        precio: req.body.precio,
+      });
+      res.redirect('/')
     },
 
-    //3
-    detail: (req,res) =>{
-        const productId = req.params.productId;
-        const productToFind = products.find((product)=> product.id == productId);
-
-        if (productToFind == undefined) {
-            return res.send("No existe el producto");
-        }
-
-        return res.render('productDetailMain', {productToFind});
+    products: function (req, res) {
+      db.Producto.findAll()
+          .then(function(productos){
+            return res.render('/products', {productos: productos});
+          })
     },
-
-    //4
-    store: (req,res) =>{
-        // capturamos los datos del form
-        const camposDeNuevoProducto = req.body;
-        console.log(camposDeNuevoProducto)
-        // asignar id a nuevo producto
-        camposDeNuevoProducto.id = 'ensalada' + Date.now();
-        // convertimos precio a number
-        camposDeNuevoProducto.precio = Number(camposDeNuevoProducto.precio);
-        camposDeNuevoProducto.imagen = '/images/'+ req.file.filename
-        // pusheamos los datos al array de objetos de js
-        products.push(camposDeNuevoProducto);
-        // pasamos de un array de objetos de js a un objeto json y lo cargamos en el data
-        fs.writeFileSync(productsFilePath, JSON.stringify(products));        
-        // redirigir la pagina
-        res.redirect('/create');
-        
+    detail: (req, res) =>{
+      db.Producto.findByPk(req.params.id, {include: [{association: 'Pedidos'}, {association: 'estados'}]})
+        .then(function(producto) {
+          return res.render('edit/', {producto:producto});
+        })
+      },
+    
+    edit: (req, res) => {    
+      let pedidoProducto = db.Producto.findByPk(req.params.id)
+      let pedidoPedido = db.Pedido.findAll();
+      Promise.all([pedidoProducto, pedidoPedido])
+        .then(function([producto, pedidos]) {
+          return res.render('edit/', {producto:producto, pedidos:pedidos});
+      })
     },
     
-    //5     
-    edit: (req, res) =>{
-      const productId = req.params.productId;
-      const productToFind = products.find((product)=> product.id == productId);
-      return res.render('edit/', {productToFind});
+
+    update: (req, res) => {  
+      db.Producto.update({
+        nombre: req.body.nombre,
+        base: req.body.base,
+        aderezo: req.body.aderezo,
+        ingredientes: req.body.ingredientes,
+        imagen: '/images/'+ req.file.filename,
+        precio: req.body.precio,
+      },{
+        where: {
+        id: req.params.id
+      }
+    });
+        res.redirect('/' + req.params.id)
     },
-      
-    //6
-    update: (req, res) => {     
-        
+    delete: (req, res) => {
+      db.Producto.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      res.redirect('/products');
+    }}
+
+    module.exports = controller;
+
+
+      // capturamos los datos del form
+      //const dataToUpdate = req.body;
+      // obtenemos el id del producto
+      //let id = dataToUpdate.id;
+      //console.log(id);
+      // filtramos el producto en el array
+      //products = products.filter(
+      //   (product) => product.id != id
+      //);
+      //console.log(products);
+      // pasamos de un array de objetos de js a un objeto json y lo cargamos en el data
+      //fs.writeFileSync(productsFilePath, JSON.stringify(products));
+      // redirigir la pagina
+      //res.redirect('/panel'); 
+    //}
+//}
+
         // => HACER barra de busqueda de productos para modificar
 
         // capturamos los datos del form
-        const dataToUpdate = req.body;
-        console.log(req.body);
+        //const dataToUpdate = req.body;
+        //console.log(req.body);
         // convertimos precio a number
-        dataToUpdate.precio = Number(dataToUpdate.precio);
+        //dataToUpdate.precio = Number(dataToUpdate.precio);
         // obtenemos el id del producto
-        let id = dataToUpdate.id;                
+        //let id = dataToUpdate.id;                
         // obtener indice del producto en el array de productos
         // products[0] = nuevo producto
-        const productIndex = products.findIndex(
-          (product) => {
-            return product.id == id;
-          })
-        if (productIndex == -1) {
-          return res.send('No existe el producto');
-        }
+        //const productIndex = products.findIndex(
+        //  (product) => {
+        //    return product.id == id;
+        //  })
+       // if (productIndex == -1) {
+       //   return res.send('No existe el producto');
+//}
         // combinamos producto existente con valores actualizados  
-        products[productIndex] = {
-          ...products[productIndex], ...dataToUpdate
-        };   
+        //products[productIndex] = {
+        //  ...products[productIndex], ...dataToUpdate
+        //};   
         // pasamos de un array de objetos de js a un objeto json y lo cargamos en el data
-        fs.writeFileSync(productsFilePath, JSON.stringify(products));
+        //fs.writeFileSync(productsFilePath, JSON.stringify(products));
         // redirigir la pagina
-        res.redirect('/panel');      
-      },  
+        //res.redirect('/panel');      
       
-    //7
-      delete: (req, res) => {
-        // capturamos los datos del form
-        const dataToUpdate = req.body;
-        // obtenemos el id del producto
-        let id = dataToUpdate.id;
-        console.log(id);
-        // filtramos el producto en el array
-        products = products.filter(
-             (product) => product.id != id
-        );
-        console.log(products);
-        // pasamos de un array de objetos de js a un objeto json y lo cargamos en el data
-        fs.writeFileSync(productsFilePath, JSON.stringify(products));
-        // redirigir la pagina
-        res.redirect('/panel'); 
-      }
-}
-
-module.exports = controller;
+    //SI FUNCIONA SAVE ELIMINAR
+    //store: (req,res) =>{
+      // capturamos los datos del form
+      //const camposDeNuevoProducto = req.body;
+      //console.log(camposDeNuevoProducto)
+      // asignar id a nuevo producto
+      //camposDeNuevoProducto.id = 'ensalada' + Date.now();
+      // convertimos precio a number
+      //camposDeNuevoProducto.precio = Number(camposDeNuevoProducto.precio);
+      //camposDeNuevoProducto.imagen = '/images/'+ req.file.filename
+      // pusheamos los datos al array de objetos de js
+      //products.push(camposDeNuevoProducto);
+      // pasamos de un array de objetos de js a un objeto json y lo cargamos en el data
+      //fs.writeFileSync(productsFilePath, JSON.stringify(products));        
+      // redirigir la pagina
+      //res.redirect('/create');
+      
+  //},
